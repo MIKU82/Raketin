aadfc78bfa3ea0fcba1915f5b943775d2d31c9f2
MIKU82 <aji@student.umn.ac.id>
2020-12-16 06:46:23 +0700

    first commit

---
+++ Raketin/Raketin/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBQgAQI1onqHu2FfR8lufxQfeTBQw-yhe0",
+    authDomain: "svcp-f75db.firebaseapp.com",
+    projectId: "svcp-f75db",
+    storageBucket: "svcp-f75db.appspot.com",
+    messagingSenderId: "828798050718",
+    appId: "1:828798050718:web:e2f715b6cae3a8c99c768f",
+
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
